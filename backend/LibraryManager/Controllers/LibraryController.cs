feb1ebc761bbaac47c58cc33e44c2fb574173b07
murilo <muriloandradegandin@gmail.com>
2026-09-18 21:16:35 -0300

    fix

---
+++ backend/LibraryManager/Controllers/LibraryController.cs
@@ -0,0 +1,11 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gestaoBiblioteca.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LibraryController : ControllerBase
+{
+}
---
+++ backend/LibraryManager/LibraryInfo.cs
@@ -0,0 +1,13 @@
+namespace gestaoBiblioteca;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Author { get; set; } = string.Empty;
+    public int Year { get; set; }
+
+    public bool Avaliable { get; set; }
+}
